e15935375a0415df2d0b9c464bd1790c27ac0044
Lee Yalkapov <gyalkapov@gmail.com>
2023-07-09 16:58:16 -0700

    added password field to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_09_230025) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_09_234943) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_09_230025) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
