2ff3f10c1bb2d4af69e8c4b95a9c650c32adbaf8
Lee Yalkapov <gyalkapov@gmail.com>
2023-07-09 12:03:29 -0700

    fixed yarn build problem with rails stimulus:manifest:update

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,6 @@
 //= link_tree ../images
-//= link_tree ../builds
 //= link_directory ../../javascript/ .js
+//= link application.css
 //= link custom.css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
