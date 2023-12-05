15aa01b2e5606c1bd116c2b413f8de5e625565f6
Lee Yalkapov <gyalkapov@gmail.com>
2023-12-04 21:00:04 -0800

    fixed flash notice in login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,19 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password]) # if user exists and password is correct
+            session[:user_id] = user.id # save user id in session
             flash[:notice] = "Logged in successfully"
             redirect_to user
         else
             flash[:alert] = "There was something wrong with your login details"
-            puts "Before render"
-            render 'new'
-            puts "After render"
+            redirect_to login_path
         end
-        
+
     end
 
     def destroy
+        session[:user_id] = nil
+        flash[:notice] = "Logged out"
+        redirect_to root_path
     end
 end
