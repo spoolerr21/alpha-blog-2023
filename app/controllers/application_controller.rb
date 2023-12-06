0de2bc77c2a1f3f2506fcd83105f23ad55a6807e
Lee Yalkapov <gyalkapov@gmail.com>
2023-12-05 21:01:54 -0800

    restricted actions for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,12 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to login_path
+      # redirect_to root_path
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
-    has_many :articles
-    validates :username, presence: true, 
+    has_many :articles, dependent: :destroy
+    validates :username, presence: true,
                         uniqueness: { case_sensitive: false },
                         length: { minimum: 3, maximum: 25 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-    validates :email, presence: true, 
+    validates :email, presence: true,
                         uniqueness: { case_sensitive: false },
                         length: { maximum: 105 },
                         format: { with: VALID_EMAIL_REGEX }
