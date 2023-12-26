8167b6ae7a45ba363307962dbd2c4f0df52f0787
Lee Yalkapov <gyalkapov@gmail.com>
2023-12-25 16:39:38 -0800

    fixed did not work, reverting last change

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -8,5 +8,5 @@ import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+// import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+// eagerLoadControllersFrom("controllers", application)
