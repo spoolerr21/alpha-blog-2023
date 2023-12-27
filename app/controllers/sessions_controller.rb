ed20e3ad11bedf614c49ee15de02afb94a981fac
Lee Yalkapov <gyalkapov@gmail.com>
2023-12-27 09:38:25 -0800

    attempt to fix 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,6 @@ class SessionsController < ApplicationController
     def destroy
         session[:user_id] = nil
         flash[:notice] = "Logged out"
-        redirect_to root_path
+        redirect_to :action => :index
     end
 end
