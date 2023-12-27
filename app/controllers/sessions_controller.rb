13e795d655922c9913b08bae52a1dd41fba6474e
Lee Yalkapov <gyalkapov@gmail.com>
2023-12-27 14:11:20 -0800

    log out fix attempt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,6 @@ class SessionsController < ApplicationController
     def destroy
         session[:user_id] = nil
         flash[:notice] = "Logged out"
-        redirect_to :action => :index
+        redirect_to root_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
   # resources :articles, except: [:destroy] # this will add all get request links automatically except destroy link
-  
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   delete 'logout', to: 'sessions#destroy'
   # we need this 2 lines for our delete link_to setup
   # delete 'articles/:id/delete' => 'articles#destroy', as: 'articles_delete'
   # get '/articles/:id/delete' => 'articles#destroy'
 end
-
