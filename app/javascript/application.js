05b10f42e21455fb8d353ebccc4eafe69e599359
Lee Yalkapov <gyalkapov@gmail.com>
2023-12-30 09:20:27 -0800

    3rd attempt to fix delete action by deactivating simulus controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // import { Turbo } from "@hotwired/turbo-rails"
 Turbo.session.drive = true
-import "./controllers"
+// import "./controllers"
 import "@hotwired/turbo-rails"
   
 console.log("Hello from app/javascript/application.js")
